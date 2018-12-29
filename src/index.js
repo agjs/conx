a912c1e7779f511ca7bd2be9fbfe976141f08c11
Aleksandar Grbic <aleksandar@airtame.com>
2018-12-29 19:58:30 +0100

    Removed irrelevant methods and updates some names

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 'use strict';
-import { detect } from 'detect-browser';
 import { ERROR_THEME, SUCCESS_THEME, WARNING_THEME, DEFAULT_COLORS } from '../src/constants';
-const browser = detect();
-/**
- * Exposing the conx class with its methods
- *
- * @param {objet} config - conx optionally takes three parameters, enabled, classes and theme
- */
+
 export default class Conx {
   constructor(group, config = {}) {
     !localStorage.getItem('conx-groups')
@@ -28,16 +22,23 @@ export default class Conx {
       };
     this.sentry();
   }
-  /**
-   * @description This method will iterate over the CSS files and look for provided CSS classes used for styling the logger.
-   * @returns colors || false
-   */
-  toggle(group) {
+
+  toggle() {
+    const isEnabled = localStorage.getItem('conx');
+    if (isEnabled) {
+      localStorage.removeItem('conx');
+    } else {
+      localStorage.setItem('conx', true);
+    }
+  }
+
+  toggleGroup(group) {
     let groups = this.groups;
     const index = groups.indexOf(group);
     index === -1 ? groups.push(group) : groups.splice(index, 1);
     localStorage.setItem('conx-groups', JSON.stringify(groups));
   }
+
   readCSSStyles() {
     const errorClass = this.classes && this.classes.error ? this.classes.error : '.conx__error';
     const successClass =
@@ -68,25 +69,30 @@ export default class Conx {
     }
     return false;
   }
+
   colorGenerator() {
     return '#' + (((1 << 24) * Math.random()) | 0).toString(16);
   }
+
   error(message) {
     if (this.enabled && this.validate(message)) {
       const err = new Error(message);
       console.error(`%c🔥  ${err.message}`, this.theme.error);
     }
   }
+
   sentry() {
     window.onerror = (message, url, lineNumber) => {
       console.error(`%cError in ${url} on line ${lineNumber} 🔥 ${message} 🔥`, this.theme.error);
       return true;
     };
   }
+
   validate(input) {
     if (input) return true;
     this.error('😭 Your logs should be explanatory, not empty.');
   }
+
   success(message) {
     if (this.enabled && this.validate(message)) {
       console.info(`%c ¯\_(ツ)_/¯ ${message}`, this.theme.success);
@@ -97,6 +103,7 @@ export default class Conx {
       console.warn(`%c ⚠ ${message}`, this.theme.warn);
     }
   }
+
   rainbow(message) {
     if (this.enabled && this.validate(message)) {
       let styles = [];
@@ -110,70 +117,7 @@ export default class Conx {
       console.log(string, ...styles);
     }
   }
-  trace(title, callback, ...args) {
-    if (this.enabled && this.validate(title) && typeof callback === 'function') {
-      let string = '';
-      if (callback.name) {
-        string = `Function ${callback.name} finished in`;
-      } else {
-        string = 'Anonymous function finished in';
-      }
-      console.groupCollapsed(`${callback.name ? `${callback.name}()` : 'Anonymous function()'}`);
-      console.time(string);
-      callback(args);
-      console.timeEnd(string);
-      console.log('Function callstack:');
-      console.log(
-        new Error().stack
-          .split('\n')
-          .reverse()
-          .slice(0, -2)
-          .reverse()
-          .join('\n')
-      );
-      console.groupEnd(`${callback.name ? `${callback.name}()` : 'Anonymous function()'}`);
-    }
-  }
-  JSONViewer(data) {
-    if (this.enabled) {
-      if (
-        JSON.parse(JSON.stringify(data)).constructor === Object ||
-        JSON.parse(JSON.stringify(data)).constructor === Array
-      ) {
-        const form = new FormData();
-        const xhttp = new XMLHttpRequest();
-        const random = Math.random()
-          .toString(36)
-          .substr(2, 5);
 
-        xhttp.onreadystatechange = function() {
-          if (this.readyState == 4 && this.status == 200) {
-            window.open(`https://codebeautify.org/jsonviewer/${this.responseText}`);
-            return;
-          } else if (this.readyState === 4 && this.status === 0) {
-            switch (browser && browser.name) {
-              case 'chrome':
-                return window.open(
-                  `https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi`
-                );
-              case 'firefox':
-                return window.open(
-                  'https://addons.mozilla.org/en-US/firefox/addon/cors-everywhere/'
-                );
-              default:
-                this.error('JSON Viewer is only supported in Chrome and Firedox.');
-            }
-          }
-        };
-
-        form.append('title', random);
-        form.append('content', JSON.stringify(data));
-        form.append('viewname', 'jsonviewer');
-        xhttp.open('POST', 'https://codebeautify.org/service/save', true);
-        xhttp.send(form);
-      }
-    }
-  }
   table(arg) {
     if (this.enabled && this.validate(arg)) {
       let table = [];
